bcefa4846ef1d3659cfb8235b217d92e4de875b0
Casey Friday <casey@friday-next.com>
2022-02-04 14:26:35 -0600

    SUPER basic stuff, just adding elements to the DOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+let city = "Luxembourg"
+let fancyCity = {
+  name: "Luxembourg",
+  country: "Somewhere near France"
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <ul>
+    <li className="cool-text">Hot Dogs</li>
+    <li>Hamburgers</li>
+    <li>Pizza</li>
+    <li>Chips</li>
+    <li id="heading">{city}, {fancyCity.country}</li>
+  </ul>,
+  document.getElementById("root")
+)

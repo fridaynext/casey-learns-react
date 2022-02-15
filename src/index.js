6699625fb14cd377b06e44d85dfd2044a8a9a7c9
Casey Friday <casey@friday-next.com>
2022-02-15 14:52:28 -0600

    First lesson

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = "Luxembourg"
-let fancyCity = {
-  name: "Luxembourg",
-  country: "Somewhere near France"
+function Hello() {
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+    </div>
+  )
 }
+
 ReactDOM.render(
-  <ul>
-    <li className="cool-text">Hot Dogs</li>
-    <li>Hamburgers</li>
-    <li>Pizza</li>
-    <li>Chips</li>
-    <li id="heading">{city}, {fancyCity.country}</li>
-  </ul>,
+  // <Hello></Hello>,
+  <Hello />,
   document.getElementById("root")
 )
